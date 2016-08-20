9563c17657104ba6f1b73c3973ebbcf137f94ec6
Douglas Shimada <shimadadouglas@gmail.com>
2016-08-20 02:08:45 -0300

    coloca enunciado dos problemas no dojo 003

--- 003/sequence.py
+++ 003/sequence.py
@@ -1,19 +1,25 @@
 """
->>> sequence(1)
-1
+Um inteiro positivo n é dado. O objetivo do problema é construir a menor
+sequencia de inteiros que termina com N de acordo com as seguintes regras:
 
->>> sequence(18)
-6
-
->>> sequence(36)
-7
-
->>> sequence(109)
-11
+1. o primeiro elemento da sequencia é 1; A[0] = 1
+2. os proximos elementos são gerados pela multiplicação do
+elemento anterior por 2 ou somando 1; A[i] = A[i-1] * 2 ou
+A[i] = A[i-1] + 1 para i >= 1.
 """
 
 
 def sequence(length):
+    """
+    >>> sequence(1)
+    1
+    >>> sequence(18)
+    6
+    >>> sequence(36)
+    7
+    >>> sequence(109)
+    11
+    """
     count = 0
     while length > 0:
         if length % 2:
--- 003/split2.py
+++ 003/split2.py
@@ -1,36 +1,44 @@
 """
->>> l = [1,5,1,1,1]
->>> countx(5, l)
-(1, 4)
->>> split(5, l)
-4
->>> split(5, [5,5,1,2,3,4,5])
-4
->>> split(5, [5,5,1,2,3,4,5])
-4
->>> split(5, [1,1,1,2,3,4,1])
-7
->>> split(5, [5,5,1,7,2,3,5])
-4
->>> split(5, [5,3,1,7,2,5,5])
-4
->>> split(5, [5,3,1,7,2,5,5])
-4
->>> split(5, [5,3,1,5,5])
-2
->>> split(5, [1,3,1,1,1])
-5
->>> split(5, [5,1,7,2,3,5])
-4
+Um numero intero x e um array A com N elementos inteiros é dado.
+Queremos saber quais elementos de A são iguas a x e quais são diferentes.
+O objetivo é dividir o array em duas partes. De tal forma que o numero de
+elementos iguais a x na primeira parte seja igual ao numero de elementos
+diferentes de x na segunda parte.
 """
 
 
 def countx(x, l):
+    """
+    >>> countx(5, [1,5,1,1,1])
+    (1, 4)
+    """
     count = l.count(x)
     return count, len(l) - count
 
 
 def split(x, l):
+    """
+    >>> split(5, [1,5,1,1,1])
+    4
+    >>> split(5, [5,5,1,2,3,4,5])
+    4
+    >>> split(5, [5,5,1,2,3,4,5])
+    4
+    >>> split(5, [1,1,1,2,3,4,1])
+    7
+    >>> split(5, [5,5,1,7,2,3,5])
+    4
+    >>> split(5, [5,3,1,7,2,5,5])
+    4
+    >>> split(5, [5,3,1,7,2,5,5])
+    4
+    >>> split(5, [5,3,1,5,5])
+    2
+    >>> split(5, [1,3,1,1,1])
+    5
+    >>> split(5, [5,1,7,2,3,5])
+    4
+    """
     curleft = 0
     left, right = countx(x, l)
     curright = right
