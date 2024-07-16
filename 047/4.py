d95be9687ac235936cdb067b0986ef12623e218a
Juliana Karoline de Sousa <julianaklulo@gmail.com>
2024-07-16 19:30:11 -0300

    Add dojo 047 (#35)
    * Add dojo 047
    
    * Add link to README

---
+++ 047/4.py
@@ -0,0 +1,22 @@
+"""
+Potência perfeita:
+Uma potência perfeita é um número positivo inteiro que pode ser expresso como a potência positiva de um número positivo. Escreva uma função que recebe um número inteiro e retorna o par (base, expoente) caso o número seja uma potência perfeita, ou retorne None caso não seja.
+
+Entrada:
+4
+
+Saída
+(2, 2)
+
+Entrada:
+9
+
+Saída
+(3, 2)
+
+Entrada:
+81
+
+Saída:
+(9, 2)
+"""
